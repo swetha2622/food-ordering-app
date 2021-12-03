e06ccb39499bfb9acc59b990cb1fd6a0e003cbf2
swetha2622 <swethasripadi1993@gmail.com>
2021-12-02 20:34:26 -0600

    available quantiry changes

--- src/components/Layout/Content/admin/Accordion.jsx
+++ src/components/Layout/Content/admin/Accordion.jsx
@@ -6,6 +6,20 @@ import Typography from '@material-ui/core/Typography';
 import {FcExpand} from 'react-icons/fc';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
+import MenuService from '../../../../services/Menu-Service';
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
+
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Chip from '@material-ui/core/Chip';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -36,6 +50,28 @@ const useStyles = makeStyles((theme) => ({
 
 const AdminAccordion = ({orders, asyncSumbitOrder}) => {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    let [menus, setMenus] = React.useState([]);
+
+    const getAvailableMenus = async (evt) => {
+        const resp = await MenuService.fetchAllItems();
+        if (resp) {
+          menus = resp;
+          setMenus(resp);
+          console.log(menus);
+          handleClickOpen();
+        } else if (resp && (resp.status === 403 || resp.status === 'error') ){
+          console.log('Something went wrong. Please try again.');
+        }
+      };
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = (value) => {
+      setOpen(false);
+    };
 
     const submitOrder = (order) => {
       const request = {
@@ -44,7 +80,17 @@ const AdminAccordion = ({orders, asyncSumbitOrder}) => {
       }
       asyncSumbitOrder(request);
     }
+    const cancelOrder = (order) => {
+      const request = {
+        ...order,
+        Status: 'cancel'  
+      }
+      asyncSumbitOrder(request);
+    }
     return (<> 
+        <Button color='darkslategray' variant='contained' onClick={getAvailableMenus}>Check Availability</Button>
+    <SimpleDialog open={open} onClose={handleClose} menus={menus}  />
+
         {orders?.map((order,index ) => {
                 return (
                 <Accordion key={`order+${index}`}>
@@ -69,6 +115,12 @@ const AdminAccordion = ({orders, asyncSumbitOrder}) => {
                         color='darkslategray' 
                         variant='contained'
                         onClick={() => submitOrder(order)}>Ready for pickup</Button>
+                        
+<Button 
+                        color='darkslategray' 
+                        variant='contained'
+                        onClick={() => cancelOrder(order)}>Cancel Order</Button>
+
                         </div>
                     </AccordionDetails>
                     { 
@@ -86,3 +138,37 @@ const AdminAccordion = ({orders, asyncSumbitOrder}) => {
 } 
 
 export default AdminAccordion;
+
+
+
+function SimpleDialog(props) {
+  const { onClose, open, menus } = props;
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Menu Availability </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+              <List sx={{ pt: 0 }}>
+                {menus.map((menu) => (
+                  <ListItem button key={menu.menu_name}>
+                    <ListItemText primary={menu.menu_name} />
+                    <Chip label={menu.availableQuantity} />
+                  </ListItem>
+                ))}
+              </List>
+        </DialogContentText>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  menus: PropTypes.array.isRequired
+};
